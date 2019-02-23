c9706d2b8ba4efb1e150a0431f408097246a7ce3
Saurabh Srivastava <saurabhsrivastava312@gmail.com>
2019-02-24 01:26:57 +0530

    First commit
    Signed-off-by: Saurabh Srivastava <saurabhsrivastava312@gmail.com>

---
+++ assets/js/custom.js
@@ -0,0 +1,45 @@
+const allClass = "active-move-up inactive-move-up active-move-down inactive-move-down";
+var canScroll = true;
+var scrollCounter = $('.screen').length;
+
+$(document).ready(function () {
+
+});
+
+$(window).bind('mousewheel', function (e) {
+    if (e.originalEvent.wheelDelta / 120 > 0)
+        scrollUpCase();
+    else
+        scrollDownCase();
+});
+
+function scrollUpCase() {
+    if (canScroll && scrollCounter < 7) {
+        scrollCounter++;
+        resetCanScroll();
+        $('.active').addClass('active-move-down').prev(".screen").addClass("inactive-move-down");
+        setTimeout(function () {
+            $('.active').removeClass('active').prev(".screen").addClass("active");
+            $(".screen").removeClass(allClass);
+        }, 1000);
+    }
+}
+
+function scrollDownCase() {
+    if (canScroll && scrollCounter > 1) {
+        scrollCounter--;
+        resetCanScroll();
+        $('.active').addClass('active-move-up').next(".screen").addClass("inactive-move-up");
+        setTimeout(function () {
+            $('.active').removeClass('active').next(".screen").addClass("active");
+            $(".screen").removeClass(allClass);
+        }, 1000);
+    }
+}
+
+function resetCanScroll() {
+    canScroll = false;
+    setTimeout(function () {
+        canScroll = true;
+    }, 2500);
+}
---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>OSS R&D</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+</head>
+<body>
+
+<main class="screen container-fluid active">
+    <div class="row">
+        <div class="col-12">
+            <div class="center-head-main">
+                <h1 class="text-center">Open Source Software Research & Development Center</h1>
+            </div>
+        </div>
+    </div>
+</main>
+
+<main class="screen container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-12 head-div-primary first">
+            <div class="center-head"><h1 class="text-center">Our Domains</h1></div>
+        </div>
+        <div class="col-lg-6 col-12 content-empty-div second"></div>
+    </div>
+</main>
+
+<main class="screen container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-12 content-empty-div second"></div>
+        <div class="col-lg-6 col-12 head-div-black first">
+            <div class="center-head"><h1 class="text-center">Our Projects</h1></div>
+        </div>
+    </div>
+</main>
+
+<main class="screen container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-12 head-div-primary first">
+            <div class="center-head"><h1 class="text-center">Our Events</h1></div>
+        </div>
+        <div class="col-lg-6 col-12 content-empty-div second"></div>
+    </div>
+</main>
+
+<main class="screen container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-12 content-empty-div second"></div>
+        <div class="col-lg-6 col-12 head-div-black first">
+            <div class="center-head"><h1 class="text-center">Centre Activity Charter</h1></div>
+        </div>
+    </div>
+</main>
+
+<main class="screen container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-12 head-div-primary first">
+            <div class="center-head"><h1 class="text-center">Our Team</h1></div>
+        </div>
+        <div class="col-lg-6 col-12 content-empty-div second"></div>
+    </div>
+</main>
+
+<main class="screen container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-12 content-empty-div second"></div>
+        <div class="col-lg-6 col-12 head-div-black first">
+            <div class="center-head"><h1 class="text-center">Our Alumini</h1></div>
+        </div>
+    </div>
+</main>
+
+<script src="assets/plugins/jquery/jquery-3.3.1.min.js"></script>
+<script src="assets/js/custom.js"></script>
+</body>
+</html>
